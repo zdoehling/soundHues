c2884198ebb2846ccf798d7596b0f6ae2db920a5
zdoehling <zeebeast50@gmail.com>
2019-09-25 09:07:57 -0400

    moving color bar left to right

--- colorBands.js
+++ colorBands.js
@@ -1,20 +1,29 @@
 const barWidth = 1;
 let lastBar = -1;
+let period = 0;
+let canvasX = 720
 
 function setup() {
-  createCanvas(720, 400);
-  colorMode(HSB, height, height, height);
+  createCanvas(canvasX, 400);
+  colorMode(HSB, height, height);
   noStroke();
   background(0);
+  frameRate(100);
 }
 
+
+
 function draw() {
-  let whichBar = mouseX / barWidth;
+  let whichBar = period / barWidth;
   if (whichBar !== lastBar) {
     let barX = whichBar * barWidth;
     fill(mouseY, height, height);
     rect(barX, 0, barWidth, height);
     lastBar = whichBar;
+    document.getElementById("position").innerHTML = period;
+    period++;
+  }
+  if(period == canvasX){
+    noLoop();
   }
 }
-
